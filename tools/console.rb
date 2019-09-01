e0c484658579bdffa545799454eda9bf389616a9
bellex0 <github email address>
2019-08-31 20:46:13 -0400

    advanced round

--- tools/console.rb
+++ tools/console.rb
@@ -9,14 +9,21 @@ end
 
 young_money = Cult.new("Young Money", "LA", 2006, "We The Best")
 free_masons = Cult.new("Free Masons", "NY", 1828, "Illuminati is watching")
+hipsters = Cult.new("Hipsters", "NY", 2009, "avocado toast is so good")
 
 wayne = Follower.new("Lil Wayne", 36, "Young Moolah baby")
 nicki = Follower.new("Nicki Minaj", 36, "My anaconda don't")
 beyonce = Follower.new("Beyonce", 37, "all the single ladies")
+jayz = Follower.new("Jay-Z", 49, "HOV")
+liam = Follower.new("Liam", 23, "is this free range chicken?")
 
 bo1 = BloodOath.new(nicki, young_money)
 bo2 = BloodOath.new(wayne, young_money)
 bo3 = BloodOath.new(beyonce, free_masons)
+bo4 = BloodOath.new(beyonce, young_money)
+bo5 = BloodOath.new(jayz, free_masons)
+bo6 = BloodOath.new(liam, hipsters)
+bo6 = BloodOath.new(liam, free_masons)
 
 
 binding.pry
